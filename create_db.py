03f3da35a2ba19f9507febb927c7e40c0a16d0bb
Anastasia <nastyaseverenkova@gmail.com>
2023-04-17 13:13:23 +0300

    Create db

---
+++ create_db.py
@@ -0,0 +1,164 @@
+from clickhouse import ClickHouse, RepoClickHouseClient
+import argparse
+from clickhub import load_config, load_types
+
+
+parser = argparse.ArgumentParser(
+    description="github importer",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+
+parser.add_argument("-c", "--config", default="config.yml", help="config")
+parser.add_argument("-d", "--debug", action="store_true", help="debug")
+
+args = parser.parse_args()
+
+config = load_config(args.config)
+types = load_types()
+
+clickhouse = ClickHouse(
+    host=config["host"],
+    port=config["port"],
+    native_port=config["native_port"],
+    username=config["username"],
+    password=config["password"],
+    secure=config["secure"],
+)
+client = RepoClickHouseClient(clickhouse)
+
+client.query_row("CREATE DATABASE IF NOT EXISTS git")
+
+client.query_row(
+    """
+CREATE TABLE git.commits
+(
+    `hash`           String,
+    `author`         LowCardinality(String),
+    `time`           DateTime,
+    `message`        String,
+    `files_added`    UInt32,
+    `files_deleted`  UInt32,
+    `files_renamed`  UInt32,
+    `files_modified` UInt32,
+    `lines_added`    UInt32,
+    `lines_deleted`  UInt32,
+    `hunks_added`    UInt32,
+    `hunks_removed`  UInt32,
+    `hunks_changed`  UInt32,
+    `repo_name`      LowCardinality(String),
+    `updated_at`     DateTime MATERIALIZED now()
+) ENGINE = ReplacingMergeTree
+ORDER BY (repo_name, time, hash)"""
+)
+
+client.query_row(
+    """
+CREATE TABLE git.file_changes
+(
+    `change_type`           Enum8('Add' = 1, 'Delete' = 2, 'Modify' = 3, 'Rename' = 4, 'Copy' = 5, 'Type' = 6),
+    `path`                  LowCardinality(String),
+    `old_path`              LowCardinality(String),
+    `file_extension`        LowCardinality(String),
+    `lines_added`           UInt32,
+    `lines_deleted`         UInt32,
+    `hunks_added`           UInt32,
+    `hunks_removed`         UInt32,
+    `hunks_changed`         UInt32,
+    `commit_hash`           String,
+    `author`                LowCardinality(String),
+    `time`                  DateTime,
+    `commit_message`        String,
+    `commit_files_added`    UInt32,
+    `commit_files_deleted`  UInt32,
+    `commit_files_renamed`  UInt32,
+    `commit_files_modified` UInt32,
+    `commit_lines_added`    UInt32,
+    `commit_lines_deleted`  UInt32,
+    `commit_hunks_added`    UInt32,
+    `commit_hunks_removed`  UInt32,
+    `commit_hunks_changed`  UInt32,
+    `repo_name`             LowCardinality(String),
+    `updated_at`            DateTime MATERIALIZED now()
+) ENGINE = ReplacingMergeTree
+ORDER BY (repo_name, time, commit_hash, path)
+SETTINGS index_granularity = 8192"""
+)
+
+client.query_row(
+    """
+CREATE TABLE git.line_changes
+(
+    `sign`                       Int8,
+    `line_number_old`            UInt32,
+    `line_number_new`            UInt32,
+    `hunk_num`                   UInt32,
+    `hunk_start_line_number_old` UInt32,
+    `hunk_start_line_number_new` UInt32,
+    `hunk_lines_added`           UInt32,
+    `hunk_lines_deleted`         UInt32,
+    `hunk_context`               LowCardinality(String),
+    `line`                       LowCardinality(String),
+    `indent`                     UInt8,
+    `line_type`                  Enum8('Empty' = 0, 'Comment' = 1, 'Punct' = 2, 'Code' = 3),
+    `prev_commit_hash`           String,
+    `prev_author`                LowCardinality(String),
+    `prev_time`                  DateTime,
+    `file_change_type`           Enum8('Add' = 1, 'Delete' = 2, 'Modify' = 3, 'Rename' = 4, 'Copy' = 5, 'Type' = 6),
+    `path`                       LowCardinality(String),
+    `old_path`                   LowCardinality(String),
+    `file_extension`             LowCardinality(String),
+    `file_lines_added`           UInt32,
+    `file_lines_deleted`         UInt32,
+    `file_hunks_added`           UInt32,
+    `file_hunks_removed`         UInt32,
+    `file_hunks_changed`         UInt32,
+    `commit_hash`                String,
+    `author`                     LowCardinality(String),
+    `time`                       DateTime,
+    `commit_message`             String,
+    `commit_files_added`         UInt32,
+    `commit_files_deleted`       UInt32,
+    `commit_files_renamed`       UInt32,
+    `commit_files_modified`      UInt32,
+    `commit_lines_added`         UInt32,
+    `commit_lines_deleted`       UInt32,
+    `commit_hunks_added`         UInt32,
+    `commit_hunks_removed`       UInt32,
+    `commit_hunks_changed`       UInt32,
+    `repo_name`                  LowCardinality(String),
+    `updated_at`                 DateTime MATERIALIZED now()
+) ENGINE = ReplacingMergeTree
+ORDER BY (repo_name, time, commit_hash, path, line_number_old, line_number_new)"""
+)
+
+client.query_row(
+    """
+CREATE TABLE git.work_queue
+(
+    `repo_name` String,
+    `scheduled` DateTime,
+    `priority` Int32,
+    `worker_id` String,
+    `started_time` DateTime,
+)
+ENGINE = KeeperMap('git_queue')
+PRIMARY KEY repo_name
+
+"""
+)
+
+client.query_row(
+    """
+CREATE TABLE git.task_queue
+(
+    `repo_name` String,
+    `scheduled` DateTime,
+    `priority` Int32,
+    `worker_id` String,
+    `started_time` DateTime,
+)
+ENGINE = KeeperMap('git_queue')
+PRIMARY KEY repo_name
+
+"""
+)
--- server/app.py
+++ server/app.py
@@ -1,55 +1,54 @@
 from flask import Flask, request, render_template
-from clickhouse import ClickHouse, RepoClickHouseClient, DataType
+from clickhouse import ClickHouse, RepoClickHouseClient
 import argparse
 from clickhub import load_config, load_types
 
 
+parser = argparse.ArgumentParser(
+    description="github importer",
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
 
-parser = argparse.ArgumentParser(description='github importer',
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-parser.add_argument('-c', '--config', default='config.yml', help='config')
-parser.add_argument('-d', '--debug', action='store_true', help='debug')
+parser.add_argument("-c", "--config", default="config.yml", help="config")
+parser.add_argument("-d", "--debug", action="store_true", help="debug")
 
 args = parser.parse_args()
 
 config = load_config(args.config)
 types = load_types()
 
-clickhouse = ClickHouse(host=config['host'], port=config['port'], native_port=config['native_port'],
-                        username=config['username'],
-                        password=config['password'], secure=config['secure'])
+clickhouse = ClickHouse(
+    host=config["host"],
+    port=config["port"],
+    native_port=config["native_port"],
+    username=config["username"],
+    password=config["password"],
+    secure=config["secure"],
+)
 client = RepoClickHouseClient(clickhouse)
 
 app = Flask(__name__)
 
-@app.get("/process")
+
+@app.get("/add_new_repo")
 def process():
-    repo = request.args.get('repo')
+    repo = request.args.get("repo")
     max_queue_size = 5
 
-    rows_in_queue = client.query_rows(types[3].statement)
-    queue_size = len(rows_in_queue)
+    queue_size = client.query_row("SELECT COUNT(repo_name) FROM work_queue")
     if queue_size == max_queue_size:
-        return 'QUEUE IS FULL', 403
-    
-    repos_in_db = 0
+        return "QUEUE IS FULL", 403
 
-    rows_in_commits = client.query_rows(types[1].statement)
-    for row in rows_in_commits:
-        if row[13] == repo:
-             repos_in_db += 1
-    
+    repos_in_db = client.query_row(
+        f"SELECT COUNT(repo_name) FROM commits WHERE repo_name = {repo}"
+    )
     if repos_in_db >= 0:
-        return 'ALREADY_PROCESSED', 200
-    
-    repos_in_queue = 0
+        return "ALREADY_PROCESSED", 200
 
-    for row in rows_in_queue:
-         if row[0] == repo:
-              repos_in_queue += 1
- 
+    repos_in_queue = client.query_row(
+        f"SELECT COUNT(repo_name) FROM work_queue WHERE repo_name = {repo}"
+    )
     if repos_in_queue >= 0:
-            return 'ALREADY_PROCESSING', 200
-    
-    return 'OK', 201
+        return "ALREADY_PROCESSING", 200
+
+    return "OK", 201
