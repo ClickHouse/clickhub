765a55c3df8a470909da8e65be64655e530c2df1
Anastasia <nastyaseverenkova@gmail.com>
2023-04-18 23:45:33 +0300

    Add dockerfile

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request
 import argparse
-
-#from clickhouse import ClickHouse, RepoClickHouseClient
-#from clickhub import load_config, load_types
+import datetime
+from clickhouse import ClickHouse, RepoClickHouseClient
+from clickhub import load_config, load_types
 from repo import importer
 
 
@@ -40,22 +40,23 @@ def process():
     if not importer.is_valid_repo(repo):
         return "BAD REQUEST", 400
     
+    repos_in_db = client.query_row(f"SELECT COUNT(repo_name) FROM commits WHERE repo_name = {repo}")
+    if repos_in_db >= 0:
+        return "ALREADY_PROCESSED", 200
+    
+    repos_in_queue = client.query_row(f"SELECT COUNT(repo_name) FROM new_queue WHERE repo_name = {repo}")
+    if repos_in_queue >= 0:
+        return "ALREADY_PROCESSING", 200
+    
     max_queue_size = args.size
 
     queue_size = client.query_row("SELECT COUNT(repo_name) FROM new_queue")
     if queue_size == max_queue_size:
         return "QUEUE IS FULL", 403
 
-    repos_in_db = client.query_row(
-        f"SELECT COUNT(repo_name) FROM commits WHERE repo_name = {repo}"
-    )
-    if repos_in_db >= 0:
-        return "ALREADY_PROCESSED", 200
-
-    repos_in_queue = client.query_row(
-        f"SELECT COUNT(repo_name) FROM new_queue WHERE repo_name = {repo}"
-    )
-    if repos_in_queue >= 0:
-        return "ALREADY_PROCESSING", 200
+    client.insert_row("git.new_queue", "repo_name", repo)
 
     return "OK", 201
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
