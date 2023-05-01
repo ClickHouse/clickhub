d8ce5066f1f23d5714a34c0d0e2a726ec0f2387b
Anastasia <nastyaseverenkova@gmail.com>
2023-05-01 19:26:52 +0300

    Add github_events table

--- create_db.py
+++ create_db.py
@@ -29,9 +29,8 @@ client = RepoClickHouseClient(clickhouse)
 
 client.query_row("CREATE DATABASE IF NOT EXISTS git")
 
-client.query_row(types[0][3])
-client.query_row(types[1][3])
-client.query_row(types[2][3])
+for i in range(3): 
+    client.query_row(types[i].statement)
 
 client.query_row(
     f"""
@@ -63,4 +62,67 @@ ENGINE = KeeperMap('git_queue')
 PRIMARY KEY repo_name
 
 """
+)
+
+client.query_row(
+    """
+    CREATE TABLE git.github_events
+(
+    `file_time` DateTime,
+    `event_type` Enum8('CommitCommentEvent' = 1, 'CreateEvent' = 2, 'DeleteEvent' = 3, 'ForkEvent' = 4, 'GollumEvent' = 5, 'IssueCommentEvent' = 6, 'IssuesEvent' = 7, 'MemberEvent' = 8, 'PublicEvent' = 9, 'PullRequestEvent' = 10, 'PullRequestReviewCommentEvent' = 11, 'PushEvent' = 12, 'ReleaseEvent' = 13, 'SponsorshipEvent' = 14, 'WatchEvent' = 15, 'GistEvent' = 16, 'FollowEvent' = 17, 'DownloadEvent' = 18, 'PullRequestReviewEvent' = 19, 'ForkApplyEvent' = 20, 'Event' = 21, 'TeamAddEvent' = 22),
+    `actor_login` LowCardinality(String),
+    `repo_name` LowCardinality(String),
+    `created_at` DateTime,
+    `updated_at` DateTime,
+    `action` Enum8('none' = 0, 'created' = 1, 'added' = 2, 'edited' = 3, 'deleted' = 4, 'opened' = 5, 'closed' = 6, 'reopened' = 7, 'assigned' = 8, 'unassigned' = 9, 'labeled' = 10, 'unlabeled' = 11, 'review_requested' = 12, 'review_request_removed' = 13, 'synchronize' = 14, 'started' = 15, 'published' = 16, 'update' = 17, 'create' = 18, 'fork' = 19, 'merged' = 20),
+    `comment_id` UInt64,
+    `body` String,
+    `path` String,
+    `position` Int32,
+    `line` Int32,
+    `ref` LowCardinality(String),
+    `ref_type` Enum8('none' = 0, 'branch' = 1, 'tag' = 2, 'repository' = 3, 'unknown' = 4),
+    `creator_user_login` LowCardinality(String),
+    `number` UInt32,
+    `title` String,
+    `labels` Array(LowCardinality(String)),
+    `state` Enum8('none' = 0, 'open' = 1, 'closed' = 2),
+    `locked` UInt8,
+    `assignee` LowCardinality(String),
+    `assignees` Array(LowCardinality(String)),
+    `comments` UInt32,
+    `author_association` Enum8('NONE' = 0, 'CONTRIBUTOR' = 1, 'OWNER' = 2, 'COLLABORATOR' = 3, 'MEMBER' = 4, 'MANNEQUIN' = 5),
+    `closed_at` DateTime,
+    `merged_at` DateTime,
+    `merge_commit_sha` String,
+    `requested_reviewers` Array(LowCardinality(String)),
+    `requested_teams` Array(LowCardinality(String)),
+    `head_ref` LowCardinality(String),
+    `head_sha` String,
+    `base_ref` LowCardinality(String),
+    `base_sha` String,
+    `merged` UInt8,
+    `mergeable` UInt8,
+    `rebaseable` UInt8,
+    `mergeable_state` Enum8('unknown' = 0, 'dirty' = 1, 'clean' = 2, 'unstable' = 3, 'draft' = 4),
+    `merged_by` LowCardinality(String),
+    `review_comments` UInt32,
+    `maintainer_can_modify` UInt8,
+    `commits` UInt32,
+    `additions` UInt32,
+    `deletions` UInt32,
+    `changed_files` UInt32,
+    `diff_hunk` String,
+    `original_position` UInt32,
+    `commit_id` String,
+    `original_commit_id` String,
+    `push_size` UInt32,
+    `push_distinct_size` UInt32,
+    `member_login` LowCardinality(String),
+    `release_tag_name` String,
+    `release_name` String,
+    `review_state` Enum8('none' = 0, 'approved' = 1, 'changes_requested' = 2, 'commented' = 3, 'dismissed' = 4, 'pending' = 5)
+)
+ENGINE = MergeTree ORDER BY (event_type, repo_name, created_at)
+SETTINGS index_granularity = 8192"""
 )
