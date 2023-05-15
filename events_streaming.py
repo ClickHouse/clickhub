ab0249315cbd12b8482d644ecb4d01f68d7fa630
Anastasia <nastyaseverenkova@gmail.com>
2023-05-16 00:22:14 +0300

    Fix bugs

--- events_streaming.py
+++ events_streaming.py
@@ -3,6 +3,7 @@ import argparse
 from clickhub import load_config
 from repo.pull_repo import pull_data, push_data, parse_data, collect_data
 import threading
+from tests import timing
 
 
 parser = argparse.ArgumentParser(
@@ -12,7 +13,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("-c", "--config", default="config.yml", help="config")
 parser.add_argument("-d", "--debug", action="store_true", help="debug")
-parser.add_argument("-s", "--size", default=100, help="batch_size")
+parser.add_argument("-s", "--size", default=10, help="batch_size")
 
 args = parser.parse_args()
 
@@ -29,24 +30,36 @@ clickhouse = ClickHouse(
 client = RepoClickHouseClient(clickhouse)
 
 
-if __name__ == "__main__":
-    repo_names = client.query_rows("SELECT DISTINCT repo_name FROM git.commits")
-    repos = [repo[0].split("/") for repo in repo_names]
-    queue = []
-
+def events_streaming(repos, lock, queue):
     for owner, repo in repos:
         url = f"https://api.github.com/repos/{owner}/{repo}/events"
         row_data, next_link, f = pull_data(url)
         data = parse_data(row_data)
-
-        lock = threading.Lock()
-
-        t1 = threading.Thread(target=collect_data(lock, queue, data))
-        t2 = threading.Thread(target=push_data(lock, client, queue, args.size))
-
-        t1.start()
-        t2.start()
+        collect_data(lock, queue, data)
 
         while f:
             row_data, next_link, f = pull_data(next_link)
             data = parse_data(row_data)
+            collect_data(lock, queue, data)
+
+
+@timing.timeit
+def main():
+    repo_names = client.query_rows("SELECT DISTINCT repo_name FROM git.commits")
+    repos = [repo[0].split("/") for repo in repo_names]
+
+    queue = []
+    lock = threading.Lock()
+
+    t1 = threading.Thread(target=events_streaming, args=(repos, lock, queue))
+    t2 = threading.Thread(target=push_data, args=(lock, client, queue, args.size))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+
+if __name__ == "__main__":
+    main()
--- repo/pull_repo.py
+++ repo/pull_repo.py
@@ -1,28 +1,31 @@
 import requests
 import json
-import threading
+import time
 
 
 def parse_link(string):
     data = string.split()
     next_link = data[0][1:-2]
-    
+
     if "last" in string:
         return next_link, True
     else:
         return "", False
 
+
 def pull_data(url):
     # User access token requests are subject to a higher limit of 15,000 requests per hour
-    data = requests.get(url, params={"per_page":100}, headers={'Authorization':"Bearer <Your token>"})
-
+    data = requests.get(
+        url, params={"per_page": 100}, headers={"Authorization": "Bearer <Your token>"}
+    )
     next_link = ""
     f = False
 
     if "Link" in data.headers.keys():
         next_link, f = parse_link(data.headers["Link"])
-    
+
     return data.text, next_link, f
+
 
 def get_value(data, path):
     for p in path:
@@ -33,14 +36,17 @@ def get_value(data, path):
             data = None
     return data
 
+
 def parse_data(data):
     data_list = json.loads(data)
 
-    structure = {"event_type":["type"],
-                 "actor_login":["actor", "login"],
-                 "repo_name":["repo", "name"],
-                 "created_at":["created_at"],
-                 "action":["payload", "action"]}
+    structure = {
+        "event_type": ["type"],
+        "actor_login": ["actor", "login"],
+        "repo_name": ["repo", "name"],
+        "created_at": ["created_at"],
+        "action": ["payload", "action"],
+    }
     res_data = {}
 
     for column in structure.keys():
@@ -48,27 +54,33 @@ def parse_data(data):
         for element in data_list:
             path = structure[column]
             res = get_value(element, path)
-            res_list.append(res) 
+            res_list.append(res)
 
         res_data[column] = res_list
 
     return res_data
 
+
 def collect_data(lock, queue, data):
     lock.acquire()
 
-    try: 
+    try:
         queue.append(data)
     finally:
         lock.release()
 
-def push_data(lock, client, queue, max_size):
-    lock.acquire()
 
-    try:
-        if len(queue) >= max_size:
-            columns, values = queue[0].keys(), [x.values() for x in queue]
-            client.insert("git.github_events", columns, values)
-            queue
-    finally:
-        lock.release()
+def push_data(lock, client, queue, max_size):
+    while True:
+        time.sleep(10)
+
+        lock.acquire()
+
+        try:
+            print(len(queue))
+            if len(queue) >= max_size:
+                columns, values = queue[0].keys(), [x.values() for x in queue]
+                client.insert_row("git.github_events", columns, values)
+                queue.clear()
+        finally:
+            lock.release()
